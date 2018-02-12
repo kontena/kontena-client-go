0041618f4de3df4ca5ac9d98bd0df6b6e6313a8f
Tero Marttila <tero.marttila@kontena.io>
2018-02-12 11:49:01 +0200

    api: refactor common idSchema

--- api/node_id.go
+++ api/node_id.go
@@ -1,10 +1,7 @@
 package api
 
 import (
-	"encoding/json"
-	"fmt"
 	"regexp"
-	"strings"
 )
 
 var NodeIDRegexp = regexp.MustCompile(`^([a-z0-9_-]+)/([a-z0-9_-]+)$`)
@@ -14,40 +11,22 @@ type NodeID struct {
 	Name string
 }
 
-func ParseNodeID(str string) (id NodeID, err error) {
-	if matches := NodeIDRegexp.FindStringSubmatch(str); matches == nil {
-		return id, fmt.Errorf("Invalid NodeID: %#v", str)
-	} else {
-		id.Grid = matches[1]
-		id.Name = matches[2]
-	}
+func (id *NodeID) schema() idSchema {
+	return idSchema{&id.Grid, &id.Name}
+}
 
-	return id, nil
+func ParseNodeID(str string) (id NodeID, err error) {
+	return id, id.schema().parse(str, NodeIDRegexp)
 }
 
 func (id *NodeID) UnmarshalJSON(buf []byte) error {
-	var str string
-
-	if err := json.Unmarshal(buf, &str); err != nil {
-		return err
-	}
-
-	parts := strings.Split(str, "/")
-
-	if len(parts) != 2 {
-		return fmt.Errorf("Invalid NodeID: %#v", str)
-	}
-
-	id.Grid = parts[0]
-	id.Name = parts[1]
-
-	return nil
+	return id.schema().unmarshalJSON(buf)
 }
 
-func (nodeID NodeID) String() string {
-	return fmt.Sprintf("%s/%s", nodeID.Grid, nodeID.Name)
+func (id NodeID) String() string {
+	return id.schema().string()
 }
 
-func (nodeID NodeID) MarshalJSON() ([]byte, error) {
-	return json.Marshal(nodeID.String())
+func (id NodeID) MarshalJSON() ([]byte, error) {
+	return id.schema().marshalJSON()
 }
